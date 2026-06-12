e319482d693d36f0eb0af66c3239933722f6df2d
Anton BV <a.biletskiy@gmail.com>
2026-06-12 15:33:27 +0200

    fix: support Plausible managed script

--- src/components/analytics/Analytics.tsx
+++ src/components/analytics/Analytics.tsx
@@ -7,17 +7,31 @@ export async function Analytics() {
   const nonce = (await headers()).get(nonceHeaderName) ?? undefined;
   const plausibleDomain = process.env.NEXT_PUBLIC_PLAUSIBLE_DOMAIN;
   const plausibleApiHost = process.env.NEXT_PUBLIC_PLAUSIBLE_API_HOST || "https://plausible.io";
+  const plausibleScriptSrc =
+    process.env.NEXT_PUBLIC_PLAUSIBLE_SCRIPT_SRC ||
+    (plausibleDomain ? `${plausibleApiHost}/js/script.js` : "");
 
   return (
     <>
-      {plausibleDomain ? (
-        <Script
-          defer
-          data-domain={plausibleDomain}
-          nonce={nonce}
-          src={`${plausibleApiHost}/js/script.js`}
-          strategy="afterInteractive"
-        />
+      {plausibleScriptSrc ? (
+        <>
+          <Script
+            async
+            data-domain={plausibleDomain}
+            nonce={nonce}
+            src={plausibleScriptSrc}
+            strategy="afterInteractive"
+          />
+          <Script
+            id="plausible-init"
+            nonce={nonce}
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html:
+                "window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};plausible.init();",
+            }}
+          />
+        </>
       ) : null}
       <VercelAnalytics />
     </>
